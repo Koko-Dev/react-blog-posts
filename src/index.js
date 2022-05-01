bf64ee917e252fd3bc2d71c5f25a9c1299b5eb1f
Koko-Dev <koko.webdev@gmail.com>
2022-05-01 13:33:55 -0400

    Create src/components directory; move App.js into that directory and change import of App.js in index.js accordingly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
