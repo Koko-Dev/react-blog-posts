f7445a7b88c417a7bc08432f0e8bfbdc0d3a169b
Koko-Dev <koko.webdev@gmail.com>
2022-05-01 14:37:29 -0400

    App.js: Change into a functional component and import react

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
-function App() {
-  return (
-    <div className="App">
-      App page
-    </div>
-  );
+import React from 'react';
+
+const App = () => {
+	return <div className="ui container">App page</div>
 }
 
 export default App;
