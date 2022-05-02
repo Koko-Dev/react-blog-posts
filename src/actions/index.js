74e140ba30299ec188032653700f1c96c3781c32
Koko-Dev <koko.webdev@gmail.com>
2022-05-02 12:30:00 -0400

    In src/actions/index.js, redux requires middleware to make a request in its action creator because the rule is that the function must return an object with type and optionally payload; when we make a request, the payload is not a javascript object, but a request, so that approach does not work; redux will not wait for the data to return;  using redux-thunk we can instead return an asynchronous function and avail ourselves of dispatch and getState; in the returned async function with arguments dispatch and getState, make the request to jsonPlaceholder '/posts' and await the response; then dispatch the action object as normal as below
    // src/actions/index.js
    import jsonPlaceholder from '../apis/jsonPlaceholder';
    
    export const fetchPosts = () => {
        // Note: With redux-thunk, we can return
        //  a function instead of an object
        return async function (dispatch, getState) {
            const response = await jsonPlaceholder.get('/posts')
            dispatch({type: 'FETCH_POSTS', payload: response})
        }
    };

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
 export const fetchPosts = () => {
-	return {
-		type: 'FETCH_POSTS'
-	};
+	// Note: With redux-thunk, we can return
+	//  a function instead of an object
+	return async function (dispatch, getState) {
+		const response = await jsonPlaceholder.get('/posts')
+		dispatch({type: 'FETCH_POSTS', payload: response})
+	}
 };
 
