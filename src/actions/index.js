43ada2c6109ca3d5485699400ecc0f05b177b2b9
Koko-Dev <koko.webdev@gmail.com>
2022-05-02 12:40:11 -0400

    Reformat src/actions/index.js fetcPosts into an arrow function; streamline syntax

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,8 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => {
-	// Note: With redux-thunk, we can return
-	//  a function instead of an object
-	return async function (dispatch, getState) {
-		const response = await jsonPlaceholder.get('/posts')
-		dispatch({type: 'FETCH_POSTS', payload: response})
-	}
-};
+export const fetchPosts = () => async dispatch => {
+	const response = await jsonPlaceholder.get('/posts')
+	dispatch({type: 'FETCH_POSTS', payload: response})
+}
+
 
