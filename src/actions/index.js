db5dd919d3ec431d1fc5a89c15a11eb483fba6a3
Koko-Dev <koko.webdev@gmail.com>
2022-05-04 15:34:04 -0400

    In src/actions/index.js, in comments, add the alternative function to action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,19 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+/*
+
+ // Note: With redux-thunk, we can return a function
+      instead of an object
+ export const fetchPosts = () => {
+	 return async function (dispatch, getState) {
+			 const response = await jsonPlaceholder.get('/posts');
+			 dispatch({type: 'FETCH_POSTS', payload: response})
+   }
+ };
+*/
+
+// Note: Action creator returns an action with the
+//  fetched data on the payload property
 export const fetchPosts = () => async dispatch => {
 	const response = await jsonPlaceholder.get('/posts')
 	dispatch({type: 'FETCH_POSTS', payload: response})
