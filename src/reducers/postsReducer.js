0a9530535ff15ed5f144e36550ee4f4e43b74e9a
Koko-Dev <koko.webdev@gmail.com>
2022-05-04 15:08:32 -0400

    In postsReducer.js, maintain a list of posts that we obtain from jsonPlaceholder; export default a reducer function with arguments state=[] and action; if action.type is 'FETCH_POSTS' from our acton creator function fetchPosts() in actions/index.js, then return the action payload; if not, simply return state;  Refactor jsonPlaceholder.js

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,6 @@
-export default () => {
-	return 123;
+// Maintain a list of posts that we obtain from JSON API
+
+export default (state = [], action) => {
+	if (action.type === 'FETCH_POSTS') return action.payload;
+	return state;
 }
