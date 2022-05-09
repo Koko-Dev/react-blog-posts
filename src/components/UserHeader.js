7ffa75b1e96a2edc4464e8f11f1ed12b8c215aec
Koko-Dev <koko.webdev@gmail.com>
2022-05-09 15:43:32 -0400

    UserHeader.js: Fix the issue of UserHeader Component receiving the entire list of users whereas, to make it more reusable, it would be better for it only to have to deal with one user at a time; move the logic of finding the user into the mapStateToProps() function which can also use the ownProps argument; the ownProps argument gives UserHeader component access to user
    // UserHeader.js
    import React from 'react';
    import {connect} from "react-redux";
    import {fetchUser} from "../actions";
    
    class UserHeader extends React.Component {
        componentDidMount() {
            this.props.fetchUser(this.props.userId);
        }
        render() {
            const { user } = this.props;
    
            if (!user) {
                return null;
            }
    
            return <div className="header">{user.name}</div>;
        }
    }
    
    const mapStateToProps = (state, ownProps) => {
        return {user: state.users.find(user => user.id === ownProps.userId)}
    }
    
    export default connect(
        mapStateToProps,
        {fetchUser}
    )(UserHeader);

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,19 +7,23 @@ class UserHeader extends React.Component {
 		this.props.fetchUser(this.props.userId);
 	}
 	render() {
-		const user = this.props.users.find(user => user.id === this.props.userId);
+		const { user } = this.props;
+
 		if (!user) {
 			return null;
 		}
+
 		return <div className="header">{user.name}</div>;
 	}
 }
 
 // Todo: Wire this up to Redux State which will then
 //  give UserHeader class component access to this.state.users
-const mapStateToProps = (state) => {
-	// console.log(state.users);
-	return {users: state.users};
+// Note: ownProps argument gives us access to props going
+//  into UserHeader Component
+const mapStateToProps = (state, ownProps) => {
+	// finds the appropriate user
+	return {user: state.users.find(user => user.id === ownProps.userId)}
 }
 
 export default connect(
