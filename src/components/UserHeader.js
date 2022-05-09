d688587ef2a051f469a93017f5c59554722a919f
Koko-Dev <koko.webdev@gmail.com>
2022-05-09 15:19:53 -0400

    UserHeader.js:  Wire up users to Redux state in order to giver UserHeader class component access to this.state.users; also, in render() create a variable whose value is the precise user which equates to this.props.userId using the find() method on this.props.users; if there is no user, simply return null; in render() return div, give a className of 'header' and display user.name; add mapStateToProps(state), which returns object {users: state.users}, to export default connect()'s first argument
    // UserHeader.js
    import React from 'react';
    import {connect} from "react-redux";
    import {fetchUser} from "../actions";
    
    class UserHeader extends React.Component {
        componentDidMount() {
            this.props.fetchUser(this.props.userId);
        }
        render() {
            const user = this.props.users.find(user => user.id === this.props.userId);
            if (!user) {
                return null;
            }
            return <div className="header">{user.name}</div>;
        }
    }
    
    const mapStateToProps = (state) => {
        // console.log(state.users);
        return {users: state.users};
    }
    
    export default connect(
        mapStateToProps,
        {fetchUser}
    )(UserHeader);

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,11 +7,22 @@ class UserHeader extends React.Component {
 		this.props.fetchUser(this.props.userId);
 	}
 	render() {
-		return <div>User Header {this.props.userId}</div>;
+		const user = this.props.users.find(user => user.id === this.props.userId);
+		if (!user) {
+			return null;
+		}
+		return <div className="header">{user.name}</div>;
 	}
 }
 
+// Todo: Wire this up to Redux State which will then
+//  give UserHeader class component access to this.state.users
+const mapStateToProps = (state) => {
+	// console.log(state.users);
+	return {users: state.users};
+}
+
 export default connect(
-	null,
+	mapStateToProps,
 	{fetchUser}
 )(UserHeader);
