9d6b1961e6b97259e2250f7e75608a086c39023c
Koko-Dev <koko.webdev@gmail.com>
2022-05-09 21:04:29 -0400

    Create a new action creator in actions/index.js, fetchPostsAndUsers(), which will dispatch action creator fetchPosts; to wire this up in PostLIst.js, import fetchPostsAndUsers from ../actions and call this.props.fetchPostsAndUsers() from componentDidMount(); also, remove all reference and import to fetchPosts() action creator and in PostList.js export default, remove fetchPosts and substitute with fetchPostsAndUsers as the mapDispatchToProps object in connect() function;
    // PostList.js
    import React from 'react';
    import {connect} from 'react-redux';
    import {fetchPostsAndUsers} from '../actions';
    import UserHeader from "./UserHeader";
    
    class PostList extends React.Component {
        componentDidMount() {
            this.props.fetchPostsAndUsers();
        }
    
        renderList() {
            return this.props.posts.map(post => {
                return (
                    <div className="item" key={post.id}>
                        <i className="large middle aligned icon user" />
                        <div className="content">
                            <div className="description">
                                <h2>{post.title}</h2>
                                <p>{post.body}</p>
                            </div>
                            <UserHeader userId={post.userId}/>
                        </div>
                    </div>
                )
            })
        }
    
        render() {
            return (
                <div className="ui relaxed divided list">
                    {this.renderList()}
                </div>
            )
        }
    }
    
    const mapStateToProps = (state) =>  {
        return { posts: state.posts };
    }
    
    export default connect(mapStateToProps,
        {
            fetchPostsAndUsers
        })(PostList);
    
    // src/actions/index.js
    import _ from 'lodash';
    import jsonPlaceholder from '../apis/jsonPlaceholder';
    
    export const fetchPostsAndUsers = () => async dispatch => {
        console.log('about to fecth posts');
        await dispatch(fetchPosts());
        console.log('fetched posts!')
    }
    
    export const fetchPosts = () => async dispatch => {
        const response = await jsonPlaceholder.get('/posts')
        dispatch({type: 'FETCH_POSTS', payload: response.data})
    }
    
    export const fetchUser = (id) => async dispatch => {
        const response = await jsonPlaceholder.get(`/users/${id}`);
        dispatch({type: 'FETCH_USER', payload: response.data});
    }

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async dispatch => {
+	console.log('about to fecth posts');
+	await dispatch(fetchPosts());
+	console.log('fetched posts!')
+
+
+}
+
 export const fetchPosts = () => async dispatch => {
 	const response = await jsonPlaceholder.get('/posts')
 	dispatch({type: 'FETCH_POSTS', payload: response.data})
@@ -8,21 +16,37 @@ export const fetchPosts = () => async dispatch => {
 
 
 // Todo: Create an action creator to fetch one user at a time
+export const fetchUser = (id) => async dispatch => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+	dispatch({type: 'FETCH_USER', payload: response.data});
+}
+
+
+//  ==========  App only uses fetchPosts and fetchUsers =========
+// export const fetchPosts = () => async dispatch => {
+// 	const response = await jsonPlaceholder.get('/posts')
+// 	dispatch({type: 'FETCH_POSTS', payload: response.data})
+// }
+//
+//
+// // Todo: Create an action creator to fetch one user at a time
+// // Solution 1
 // export const fetchUser = (id) => async dispatch => {
 // 	const response = await jsonPlaceholder.get(`/users/${id}`);
 // 	dispatch({type: 'FETCH_USER', payload: response.data});
 // }
 
 
+// // Solution 2
 // Use lodash to minimize requests = 23 requests
-export const fetchUser = _.memoize(function (id) {
-	return _.memoize(async function (dispatch) {
-		const response = await jsonPlaceholder.get(`/users/${id}`);
-		dispatch({type: 'FETCH_USER', payload: response.data});
-	})
-})
+// export const fetchUser = _.memoize(function (id) {
+// 	return _.memoize(async function (dispatch) {
+// 		const response = await jsonPlaceholder.get(`/users/${id}`);
+// 		dispatch({type: 'FETCH_USER', payload: response.data});
+// 	})
+// })
 
-
+// // Solution 3
 // Use lodash to minimize API requests == 23 requests
 // export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
 //
