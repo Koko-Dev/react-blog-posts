1b8b88221707bdcae4f0d9b119b329c6606f9a67
Koko-Dev <koko.webdev@gmail.com>
2022-05-09 20:39:16 -0400

    In src/actions/index.js, refactor the fetchUser memoized solution and note that it also makes 23 requests like the last one, but is still much less than the initial 123 requests; these last two solutions are good as long as we do not need to fetch a unique user more than once; in the event that we need to fetch a unique user more than once, we would have to create another action creator altogether; in this app we should not need to
    // src/actions/index.js fetchUser using lodash
    
    // Todo: Create an action creator to fetch one user at a time
    // export const fetchUser = (id) => async dispatch => {
    //  const response = await jsonPlaceholder.get(`/users/${id}`);
    //  dispatch({type: 'FETCH_USER', payload: response.data});
    // }
    
    // Use lodash to minimize requests = 23 requests
    export const fetchUser = _.memoize(function (id) {
        return _.memoize(async function (dispatch) {
            const response = await jsonPlaceholder.get(`/users/${id}`);
            dispatch({type: 'FETCH_USER', payload: response.data});
        })
    })
    
    // Use lodash to minimize API requests == 23 requests
    // export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
    //
    // const _fetchUser = _.memoize(async (id, dispatch) => {
    //  const response = await jsonPlaceholder.get(`/users/${id}`);
    //  dispatch({type: 'FETCH_USER', payload: response.data});
    // })

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,11 +13,22 @@ export const fetchPosts = () => async dispatch => {
 // 	dispatch({type: 'FETCH_USER', payload: response.data});
 // }
 
+
+// Use lodash to minimize requests = 23 requests
 export const fetchUser = _.memoize(function (id) {
 	return _.memoize(async function (dispatch) {
 		const response = await jsonPlaceholder.get(`/users/${id}`);
 		dispatch({type: 'FETCH_USER', payload: response.data});
 	})
 })
+
+
+// Use lodash to minimize API requests == 23 requests
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+//
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+// 	const response = await jsonPlaceholder.get(`/users/${id}`);
+// 	dispatch({type: 'FETCH_USER', payload: response.data});
+// })
 
 
